f93069484dc4708d75acba4648b9399557839e21
TimeSpeeding <47582010+TimeSpeeding@users.noreply.github.com>
2019-05-22 16:36:30 +0800

    Add files via upload

---
+++ CA/LAPS/src/main/java/com/t13/dva/LAPS/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.t13.dva.LAPS.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.t13.dva.LAPS.model.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	User findByUsername(String username);
+
+}
