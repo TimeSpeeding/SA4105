490192c3e4c0d748153e7cb25ecae145998cde33
TimeSpeeding <andrewzhu0328@163.com>
2019-05-23 10:01:57 +0800

    .

--- CA/LAPS/src/main/java/com/t13/dva/LAPS/controller/LoginController.java
+++ CA/LAPS/src/main/java/com/t13/dva/LAPS/controller/LoginController.java
@@ -1,19 +1,12 @@
 package com.t13.dva.LAPS.controller;
 
-import java.io.Console;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.t13.dva.LAPS.model.User;
@@ -25,31 +18,11 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+	@RequestMapping(value = {"/","/login"}, method = RequestMethod.GET)
 	public String login() {
 		System.out.println("login page");
 		return "login";
 	}
-	
-//	@RequestMapping(value = {"/login"}, method = RequestMethod.POST)
-//	public String emmm(@RequestParam String username, @RequestParam String password, Model model, HttpServletRequest request) {
-//		int s = userService.checkpassword(username, password);
-//		System.out.println(s);
-//		if (s == 1) {
-//			request.getSession().setAttribute("name", username);
-//			return "/hello";
-//		} else if (s == 2) {
-//			request.getSession().setAttribute("name", username);
-//			return "/hello";
-//		} else if (s == 3) {
-//			request.getSession().setAttribute("name", username);
-//			return "/staff/home";
-//		} else {
-//			model.addAttribute("error", "enter right username/password");
-//			return "index";
-//		}
-//		
-//	}
 	
 	@RequestMapping(value= "/registration", method = RequestMethod.GET)
     public ModelAndView registration(){
@@ -97,16 +70,6 @@ public class LoginController {
 		return "manager/hello";
 	}
 	
-//	@RequestMapping(value="/admin/home", method = RequestMethod.GET)
-//    public ModelAndView home(){
-//        ModelAndView modelAndView = new ModelAndView();
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        User user = userService.findUserByUsername(auth.getName());
-//        modelAndView.addObject("userName", "Welcome " + user.getName()  + " (" + user.getEmail() + ")");
-//        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-//        modelAndView.setViewName("admin/home");
-//        return modelAndView;
-//    }
 	
 	
 }
